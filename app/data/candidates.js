196d63424806e6f989c4e0eb686737f163428ec2
Savannah Israel <sisrael0405@gmail.com>
2019-07-20 23:03:39 -0600

    candidate pictures and websites

--- app/data/candidates.js
+++ app/data/candidates.js
@@ -2,8 +2,8 @@ var candidates = [
     {
         routeName: "berniesanders",
         name: "Bernie Sanders",
-        photo: "",
-        website: "",
+        photo: "https://images.dailykos.com/images/199601/large/01-bernie-sanders-happy.w529.h529.jpg?1454054028",
+        website: "https://berniesanders.com/",
         scores: [
             1,
             2,
@@ -20,8 +20,8 @@ var candidates = [
     {
         routeName: "elizabethwarren",
         name: "Elizabeth Warren",
-        photo: "",
-        website: "",
+        photo: "https://amp.businessinsider.com/images/5c990a586b526549100aec56-750-375.jpg",
+        website: "https://elizabethwarren.com",
         scores: [
             5,
             4,
@@ -32,6 +32,114 @@ var candidates = [
             2,
             3,
             4,
+            5
+        ]
+    },
+    {
+        routeName: "petebuttigieg",
+        name: "Pete Buttigieg",
+        photo: "https://pmcdeadline2.files.wordpress.com/2019/04/pete-buttigieg.jpg",
+        website: "https://peteforamerica.com/",
+        scores: [
+            5,
+            2,
+            3,
+            4,
+            1,
+            5,
+            3,
+            3,
+            1,
+            3
+        ]
+    },
+    {
+        routeName: "kamalaharris",
+        name: "Kamala Harris",
+        photo: "https://amp.businessinsider.com/images/5c8c29f1f73f5c5f7e5c6853-750-375.jpg",
+        website: "https://kamalaharris.org/",
+        scores: [
+            1,
+            1,
+            1,
+            2,
+            5,
+            5,
+            5,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        routeName: "joebiden",
+        name: "Joe Biden",
+        photo: "https://peopledotcom.files.wordpress.com/2018/11/joe-biden.jpg",
+        website: "https://joebiden.com/",
+        scores: [
+            5,
+            5,
+            3,
+            5,
+            1,
+            1,
+            1,
+            1,
+            1,
+            5
+        ]
+    },
+    {
+        routeName: "andrewyang",
+        name: "Andrew Yang",
+        photo: "https://cdn.vox-cdn.com/thumbor/OI8ULB1VpTqL4HmFz0yh1j60WR0=/0x0:2040x1360/1200x675/filters:focal(857x517:1183x843)/cdn.vox-cdn.com/uploads/chorus_image/image/63572227/akrales_190411_3346_0152.0.jpg",
+        website: "https://www.yang2020.com/",
+        scores: [
+            4,
+            4,
+            3,
+            3,
+            2,
+            1,
+            1,
+            5,
+            2,
+            2
+        ]
+    },
+    {
+        routeName: "mariannewilliamson",
+        name: "Marianne Williamson",
+        photo: "https://ichef.bbci.co.uk/news/660/cpsprodpb/13DFE/production/_107660418_gettyimages-1158728706.jpg",
+        website: "https://www.marianne2020.com",
+        scores: [
+            5,
+            2,
+            3,
+            2,
+            5,
+            1,
+            2,
+            5,
+            4,
+            3
+        ]
+    },
+    {
+        routeName: "donaldtrump",
+        name: "Donald Trump",
+        photo: "https://media.gq.com/photos/5d123d01e7b6f99084ac63db/16:9/w_1280,c_limit/president-Trump's-Insults-of-Sexual-Assault-Victims-gq.jpg",
+        website: "https://www.donaldjtrump.com/",
+        scores: [
+            5,
+            1,
+            5,
+            1,
+            5,
+            1,
+            5,
+            3,
+            1,
             5
         ]
     }
